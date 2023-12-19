4d7df4930435f27945d736bdce9905ed8604feb4
Юлия <juliya_dsk@mail.ru>
2023-12-19 21:10:32 +0300

    добавили решение второй задачи

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.
+
+int[] myArray = { 1, 8, 43, 4, 55, 60, 3, 2, 1, 3 };
+int count = 0;
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            int num = myArray[i]; 
+           if (num % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Количество четных чисел в массиве: {count}");
